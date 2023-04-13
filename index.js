c043fefcbddfb29382a6476ec32f06847de64a9a
mak-gok <mak-gok@mail.ru>
2023-04-13 22:07:12 +1000

    jfhfhfhf

---
+++ index.js
@@ -0,0 +1,37 @@
+// let intenger
+// let string = 'hello'
+// let boolean = false
+// let float = 8.9874
+
+//  integer = 1000
+
+//  console.log(integer);
+  let btn = document.getElementById('button')
+  console.log(btn);
+  btn.onclick = ()=>{
+    console.log('Дегустация');
+    }
+//     let age=prompt("введите ваш возраст")
+//     console.log(age);
+//   if (age<18 && age>10) {
+//     console.log("условие верно");
+//   }
+//   if(age>0 && age<10)
+// console.log("младенец");  
+
+//   else{
+//     console.log(("условие не выполнено"));
+//   }
+//   let num=prompt("введите число")
+//   console.log(num);
+//   if(num<10 || num>100)
+//   console.log("win");
+for (let i = 10; i > 0; i=i-2) {
+ console.log("hi");;
+  console.log(i);
+}
+let a=0
+while (a<10) {
+  console.log(a); 
+  a++
+}
